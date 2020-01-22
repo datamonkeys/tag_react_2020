8eca72f15b7ad3253daec61564695cc303831841
Louis Perner <louisperner@gmail.com>
2020-01-22 14:35:39 +0100

    offline_chat: Messages

--- offline_chat/src/App.js
+++ offline_chat/src/App.js
@@ -9,13 +9,26 @@ import Messages from "./components/Messages";
 import Input from "./components/Input";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    messages: []
+  };
+
+  messageToState = whereverVarName => {
+    console.log("app", whereverVarName);
+    const messages = this.state.messages;
+
+    messages.push(whereverVarName);
+
+    this.setState({
+      messages
+    });
+  };
 
   render() {
     return (
       <div className="App container">
         <Messages />
-        <Input />
+        <Input parentMsgToState={this.messageToState} />
       </div>
     );
   }
--- offline_chat/src/components/Input.js
+++ offline_chat/src/components/Input.js
@@ -1,28 +1,72 @@
 // React Imports
 import React from "react";
 
-function Input() {
-  return (
-    <form>
-      <div className="form-row">
-        <div className="col">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
+class Input extends React.Component {
+  state = {
+    inputName: "",
+    inputMessage: ""
+  };
+
+  handleInputName = event => {
+    const inputName = event.target.value;
+
+    this.setState({
+      inputName
+    });
+  };
+
+  handleInputMessage = event => {
+    const inputMessage = event.target.value;
+
+    this.setState({
+      inputMessage
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const obj = {
+      name: this.state.inputName,
+      msg: this.state.inputMessage
+    };
+
+    console.log("obj", obj);
+
+    this.props.parentMsgToState(obj);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-row">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              onChange={this.handleInputName}
+              value={this.state.inputName}
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Message"
+              onChange={this.handleInputMessage}
+              value={this.state.inputMessage}
+            />
+          </div>
+          <div className="col">
+            <button type="submit" className="btn btn-primary  btn-block">
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="col">
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-        <div className="col">
-          <button type="submit" class="btn btn-primary  btn-block">
-            Submit
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+      </form>
+    );
+  }
 }
 
 export default Input;
