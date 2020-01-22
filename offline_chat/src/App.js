6c657ace4531d8ffce55fe2964f23a18819e2388
Louis Perner <louisperner@gmail.com>
2020-01-22 11:58:25 +0100

    offline_chat: components basic

--- offline_chat/src/App.js
+++ offline_chat/src/App.js
@@ -2,21 +2,18 @@
 import React from "react";
 
 // Styles
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 // Components
 import Messages from "./components/Messages";
 import Input from "./components/Input";
 
-import { Container } from "react-bootstrap";
-
 class App extends React.Component {
   state = {};
 
   render() {
     return (
-      <div className="App">
+      <div className="App container-fluid">
         <Messages />
         <Input />
       </div>
--- offline_chat/src/components/Input.js
+++ offline_chat/src/components/Input.js
@@ -1,8 +1,6 @@
 // React Imports
 import React from "react";
 
-import { Row, Form, Col, Button } from "react-bootstrap";
-
 function Input() {
   return <div>Inputs</div>;
 }
