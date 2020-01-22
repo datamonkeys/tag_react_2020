814adc3149e8fe7dd7d9cc582610ee5b65db4881
Louis Perner <louisperner@gmail.com>
2020-01-22 11:39:18 +0100

    offline_chat: components basic

--- offline_chat/src/components/Input.js
+++ offline_chat/src/components/Input.js
@@ -4,22 +4,7 @@ import React from "react";
 import { Row, Form, Col, Button } from "react-bootstrap";
 
 function Input() {
-  return (
-    <Row>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    </Row>
-  );
+  return <div>Inputs</div>;
 }
 
 export default Input;
--- offline_chat/src/components/Messages.js
+++ offline_chat/src/components/Messages.js
@@ -1,30 +1,14 @@
 // React Imports
 import React from "react";
 
-import { Row, Table } from "react-bootstrap";
+import Message from "./Message";
 
 function Messages() {
   return (
-    <Row>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
-    </Row>
+    <div>
+      Messages
+      <Message />
+    </div>
   );
 }
 
